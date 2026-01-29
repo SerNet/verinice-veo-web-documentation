cfb9e1d3fc6633a53eb84a704ba657a32c444bc8
Haneen <hh@sernet.de>
2026-01-29 08:51:07 +0100

    ensure document has a single main landmark
    verinice-veo#4507

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
-import { useData } from 'vitepress'
+import { inBrowser, useData } from 'vitepress'
 import { watchEffect } from 'vue'
 
 const { lang } = useData()
